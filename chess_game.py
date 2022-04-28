badcb2db37527745a3ded168b88ad2ca6cfaabd5
Arturo Joya <misael.joya.mj@gmail.com>
2022-04-28 13:20:34 -0400

    Create Main function Creates the main function for basic chess gameplay. To quit the game, the user must manually input a quit. Otherwise, it will be an infinitely running game of chess.

--- chess_game.py
+++ chess_game.py
@@ -1,11 +1,39 @@
-"""
+."""
 Main program to set up and run a game of chess.
 """
 from chess_board import ChessBoard
+from chess_pieces import Pawn Rook Bishop Knight Queen King
+from chess_view import TextView
+from chess_controller import TextController
 
 def main():
     """
     Main function to run a chess game.
+    
+    The current main function runs infinitely until a player choses to quit the
+    game.
     """
+    #set up MVC components for an instance of chess
     board = ChessBoard()
-    print(board._board)
+    view = TextView(board)
+    controllers = {
+        white: TextController(board),
+        black: TextController(board),
+    }
+    current_player = None
+    
+    #TODO find a win condition so this isn't an infinite loop
+    while True:
+        if board.next_player():
+            current_player = white
+        else:
+            current_player = black
+        if controllers[current_player].quit():
+            break
+        view.draw()
+        controllers[current_player].move()
+        view.draw()
+    print(f"{current_player" has ended the game.")
+
+if __name__ == "__main__":
+    main()
