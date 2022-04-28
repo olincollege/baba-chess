f49abb197595504af4c8f8e9255cdc55d97cb908
Drew P <silverbird337@gmail.com>
2022-04-28 12:08:12 -0400

    Modify chess_game for board class testing

--- chess_game.py
+++ chess_game.py
@@ -1,3 +1,11 @@
 """
 Main program to set up and run a game of chess.
 """
+from chess_board import ChessBoard
+
+def main():
+    """
+    Main function to run a chess game.
+    """
+    board = ChessBoard()
+    print(board._board)
