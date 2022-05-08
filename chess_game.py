27cc0650aef4f27b5a0d1f090f0cc8fdbec4b637
Arturo Joya <misael.joya.mj@gmail.com>
2022-05-07 22:53:25 -0400

    Standardize LAN Standardizes LAN language across these files, specifically in the print- outs

--- chess_game.py
+++ chess_game.py
@@ -22,19 +22,18 @@ def main():
     current_player = None
     #Print out a set of rules and allowed inputs
     print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n"\
-          "Hello and Welcome to Baba-Chess 1.0, a simple text-based Python\n"\
+          "Hello and Welcome to Baba-Chess 1.2, a simple text-based Python\n"\
           "implementation of a chess board. In order to make a\n"\
           "move on the board, please enter your moves in Long Algebraic\n"\
-          "Notation (ie. e2, e4). When prompted to move, there are three valid"\
-          " movetypes.\nMake a move - e2, e4\nQuit - quit\n"\
-          "Undo move - undo\nThe current revision of this chess board relies "\
-          "heavily on your knowledge of chess.\nNotably, enpassant captures, "\
+          "Notation (ie. e2-e4). When prompted to move, there are three valid"\
+          " movetypes.\nMake a move - `e2-e4`\nQuit - `quit`\n"\
+          "Undo move - `undo`\nThe current revision of this chess board relies"\
+          " on your knowledge of chess.\nNotably, enpassant captures, "\
           "castling, and promotions have not been implemented into this "\
           "version;\nhowever, progress is being made towards improving this "\
           "chess board.\n"\
           "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
 
-    #TODO find a win condition so this isn't an infinite loop
     while True:
         current_player = controllers[(board.next_player())]
         view.draw()
@@ -51,17 +50,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# def board_move_test():
-#     #set up MVC components for an instance of chess
-#     board = ChessBoard()
-#     view = TextView(board)
-#     controllers = {
-#         "white": TextController(board),
-#         "black": TextController(board),
-#     }
-#     current_player = controllers[(board.next_player())]
-#     print(board.is_occupied((5, 0)))
-#     print(board.piece_color(board.get_piece((7,7))))
-
-# board_move_test()
