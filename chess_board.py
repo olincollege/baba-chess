cba537a263e13f57bf80cb8390996a5f388b00fb
Drew P <silverbird337@gmail.com>
2022-05-08 05:24:25 -0400

    Move methods outside of the ChessBoard class
    Both the in_bound and get_piece_color do not reference self and were
    moved outside of the ChessBoard class.

--- chess_board.py
+++ chess_board.py
@@ -4,6 +4,40 @@ Chess Board Implementation.
 
 from selectors import PollSelector
 
+def _in_bound(pos):
+    """
+    Return a bool based on if a position is on the board.
+
+    Args:
+        pos: A tuple representing the position of a given square. The first
+        number is the row, the second is the column.
+
+    Returns:
+        A bool representing if a position is on the board. True if it is,
+        false if it is not.
+    """
+    if pos[0] in range(0,8):
+        if pos[1] in range(0,8):
+            return True
+    return False
+
+def get_piece_color(piece):
+    """
+    Return the color of a piece in a given square.
+
+    Args:
+        piece: A string representing a chess piece.
+
+    Returns:
+        A string representing the color of a given piece. "white" if the
+        piece is white, "black" if the piece is black, and "blank" if
+        there is no piece at the given square.
+    """
+    if piece == " ":
+        return "blank"
+    if piece.isupper():
+        return "white"
+    return "black"
 
 class ChessBoard:
     """
@@ -15,8 +49,10 @@ class ChessBoard:
         blank_square: A string representing a blank square symbol.
         _board: A list of lists representing the squares of the board.
         _next_player: A string representing the color of the player to move.
-        _moves_made: A list of tuples representing the moves that have been made.
-        _pieces_captured: A list of single character strings representing the pieces that have been captured.
+        _moves_made: A list of tuples representing the moves that have been
+            made.
+        _pieces_captured: A list of single character strings representing the
+            pieces that have been captured.
         _undo_flag: An integer representing the undo state of a move.
     """
 
@@ -43,11 +79,13 @@ class ChessBoard:
         represent black pieces. Strings represent pieces as the following:
         "R" = Rook, "N" = Knight, "B" = Bishop, "Q" = Queen, and "K" = King.
         """
-        self._board[7] = ["R", "N", "B", "Q", "K", "B", "N", "R"] # White back row
-        self._board[6] = ["P" for _ in range (8)] # White pawns
-        
-        self._board[1] = ["p" for _ in range(8)] # Black pawns
-        self._board[0] = ["r", "n", "b", "q", "k", "b", "n", "r"] # Black back row
+        # White pieces
+        self._board[7] = ["R", "N", "B", "Q", "K", "B", "N", "R"]
+        self._board[6] = ["P" for _ in range (8)]
+
+        # Black pieces
+        self._board[1] = ["p" for _ in range(8)]
+        self._board[0] = ["r", "n", "b", "q", "k", "b", "n", "r"]
 
     def next_player(self):
         """
@@ -57,7 +95,7 @@ class ChessBoard:
             A string representing the next player's color (white or black).
         """
         return self._next_player
-    
+
     def _flip_next_move(self):
         """
         Change the next player to move.
@@ -74,47 +112,12 @@ class ChessBoard:
         Args:
             pos: A tuple representing the position of a given square. The first
             number is the row, the second is the column.
-        
+
         Returns:
             A string representing the piece at a given square. An empty string
             is returned if a square is empty.
         """
         return self._board[pos[0]][pos[1]]
-
-    def _in_bound(self, pos):
-        """
-        Return a bool based on if a position is on the board.
-
-        Args:
-            pos: A tuple representing the position of a given square. The first
-            number is the row, the second is the column.
-
-        Returns:
-            A bool representing if a position is on the board. True if it is,
-            false if it is not. 
-        """
-        if pos[0] in range(0,8):
-            if pos[1] in range(0,8):
-                return True
-        return False
-
-    def piece_color(self, piece):
-        """
-        Return the color of a piece in a given square.
-
-        Args:
-            piece: A string representing a chess piece.
-        
-        Returns:
-            A string representing the color of a given piece. "white" if the
-            piece is white, "black" if the piece is black, and "blank" if
-            there is no piece at the given square. 
-        """
-        if piece == " ":
-            return "blank"
-        if piece.isupper():
-            return "white"
-        return "black"
 
     def _opponent_color(self, piece):
         """
@@ -126,7 +129,7 @@ class ChessBoard:
         Returns:
             A string representing the opponent color of a given piece. "white"
             if the piece is white, "black" if the piece is black, and "blank" if
-            there is no piece at the given square. 
+            there is no piece at the given square.
         """
         if piece == " ":
             return "blank"
@@ -147,12 +150,12 @@ class ChessBoard:
         """
         piece = self.get_piece(start_pos)
 
-        if self.piece_color(piece) != self._next_player:
+        if get_piece_color(piece) != self._next_player:
             print("Not your own piece!")
             raise ValueError
 
         # Check if move will capture your own piece.
-        if self.piece_color(self.get_piece(end_pos)) == self.piece_color(piece):
+        if get_piece_color(self.get_piece(end_pos)) == get_piece_color(piece):
             print("You cannot capture your own piece!")
             raise ValueError
 
@@ -187,18 +190,18 @@ class ChessBoard:
         col = start_pos[1]
         moves = []
         piece = self.get_piece(start_pos)
-        if self.piece_color(piece) == "white":
+        if get_piece_color(piece) == "white":
             if row == 6: #if first move, check the second square ahead
                 if self.get_piece((row - 2, col)) == " " and \
                     self.get_piece((row - 1, col)) == " ":
-                        moves.append((row - 2, col))
+                    moves.append((row - 2, col))
             for i in range(-1,2):
                 test_pos = (row - 1, col + i)
-                if self._in_bound(test_pos):
+                if _in_bound(test_pos):
                     if i == 0:
                         if self.get_piece(test_pos) == " ":
                             moves.append(test_pos)
-                    elif self.piece_color(self.get_piece(test_pos)) == "black":
+                    elif get_piece_color(self.get_piece(test_pos)) == "black":
                         moves.append(test_pos)
         else:
             if row == 1:
@@ -207,11 +210,11 @@ class ChessBoard:
                     moves.append((row + 2, col))
             for i in range(-1,2):
                 test_pos = (row + 1, col + i)
-                if self._in_bound(test_pos):
+                if _in_bound(test_pos):
                     if i == 0:
                         if self.get_piece(test_pos) == " ":
                             moves.append(test_pos)
-                    elif self.piece_color(self.get_piece(test_pos)) == "white":
+                    elif get_piece_color(self.get_piece(test_pos)) == "white":
                         moves.append(test_pos)
         return moves
 
@@ -229,16 +232,18 @@ class ChessBoard:
         row = start_pos[0]
         col = start_pos[1]
         orth = [(1,0), (-1,0), (0,1), (0,-1)] # define directions for movement
-        piece_color = self.piece_color(self.get_piece(start_pos))
+        piece_color = get_piece_color(self.get_piece(start_pos))
         for direction in orth:
             for i in range(1,8):
                 test_pos = (row + i*direction[0], col + i*direction[1])
-                if self._in_bound(test_pos):
+                if _in_bound(test_pos):
                     if self.get_piece(test_pos) == " ":
                         moves.append(test_pos)
-                    if self.piece_color(self.get_piece(test_pos)) == piece_color:
+                    if get_piece_color(self.get_piece(test_pos)) == \
+                        piece_color:
                         break
-                    if self.piece_color(self.get_piece(test_pos)) not in ["blank", piece_color]:
+                    if get_piece_color(self.get_piece(test_pos)) not in \
+                        ["blank", piece_color]:
                         moves.append(test_pos)
                         break
                 else:
@@ -262,8 +267,9 @@ class ChessBoard:
         opp_color = self._opponent_color(self.get_piece(start_pos))
         for direction in in_L:
             test_pos = (row + direction[0], col + direction[1])
-            if self._in_bound(test_pos):
-                if self.piece_color(self.get_piece(test_pos)) in ["blank", opp_color]:
+            if _in_bound(test_pos):
+                if get_piece_color(self.get_piece(test_pos)) in \
+                    ["blank", opp_color]:
                     moves.append(test_pos)
         return moves
 
@@ -280,17 +286,19 @@ class ChessBoard:
         moves = []
         row = start_pos[0]
         col = start_pos[1]
-        piece_color = self.piece_color(self.get_piece(start_pos))
+        piece_color = get_piece_color(self.get_piece(start_pos))
         diag = [(1,1), (-1,1), (1,-1), (-1,-1)] # define directions for movement
         for direction in diag:
             for i in range(1,8):
                 test_pos = (row + i*direction[0], col +i*direction[1])
-                if self._in_bound(test_pos):
+                if _in_bound(test_pos):
                     if self.get_piece(test_pos) == " ":
                         moves.append(test_pos)
-                    if self.piece_color(self.get_piece(test_pos)) == piece_color:
+                    if get_piece_color(self.get_piece(test_pos)) == \
+                        piece_color:
                         break
-                    if self.piece_color(self.get_piece(test_pos)) not in ["blank", piece_color]:
+                    if get_piece_color(self.get_piece(test_pos)) not in \
+                        ["blank", piece_color]:
                         moves.append(test_pos)
                         break
                 else:
@@ -313,16 +321,18 @@ class ChessBoard:
         orth = [(1,0), (-1,0), (0,1), (0,-1)] # define directions for movement
         diag = [(1,1), (-1,1), (1,-1), (-1,-1)]
         full = orth + diag
-        piece_color = self.piece_color(self.get_piece(start_pos))
+        piece_color = get_piece_color(self.get_piece(start_pos))
         for direction in full:
             for i in range(1,8):
                 test_pos = (row + i*direction[0], col + i*direction[1])
-                if self._in_bound(test_pos):
+                if _in_bound(test_pos):
                     if self.get_piece(test_pos) == " ":
                         moves.append(test_pos)
-                    if self.piece_color(self.get_piece(test_pos)) == piece_color:
+                    if get_piece_color(self.get_piece(test_pos)) == \
+                        piece_color:
                         break
-                    if self.piece_color(self.get_piece(test_pos)) not in ["blank", piece_color]:
+                    if get_piece_color(self.get_piece(test_pos)) not in \
+                        ["blank", piece_color]:
                         moves.append(test_pos)
                         break
                 else:
@@ -348,9 +358,10 @@ class ChessBoard:
         opp_color = self._opponent_color(self.get_piece(start_pos))
         for direction in full:
             test_pos = (row + direction[0], col + direction[1])
-            if self._in_bound(test_pos):
-                if self.piece_color(self.get_piece(test_pos)) in ["blank", opp_color]:
-                        moves.append(test_pos)
+            if _in_bound(test_pos):
+                if get_piece_color(self.get_piece(test_pos)) in \
+                    ["blank", opp_color]:
+                    moves.append(test_pos)
         return moves
 
     def move_piece(self, start_pos, end_pos):
@@ -370,7 +381,7 @@ class ChessBoard:
         if self._undo_flag == 0:
             # All-in-one legal move checker.
             self.check_legal_move(start_pos, end_pos)
-        
+
         # Move piece and replace previous square with a blank square.
         self._board[start_pos[0]][start_pos[1]] = self.blank_square
         #if a piece is captured, add it to the pieces_captured list.
@@ -397,7 +408,7 @@ class ChessBoard:
         """
         Return a string representing the contents of the board.
         """
-        files = "   a b c d e f g h" 
+        files = "   a b c d e f g h"
         # files = "   0 1 2 3 4 5 6 7"
         row_divider = "  " + ("+-" * 8) + "+"
         lines = [files, row_divider]
